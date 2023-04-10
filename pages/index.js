694d6eeb78dc26166e61804781f52017f2273b77
bobo <bo930236@gmail.com>
2023-04-10 17:57:22 +0800

    update personal informations

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home({ posts }) {
                 animationDuration={1500}
                 className="text-slate-200"
               >
-                thoughts, records & everything
+                thoughts, records & everything&nbsp;
               </RoughNotation>
               in between. Have a good read!{' '}
               <div className="mt-8 text-slate-600 dark:text-slate-400">
@@ -78,7 +78,7 @@ export default function Home({ posts }) {
               <div className="my-2 grid items-start gap-8">
                 <div className="group relative">
                   <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <Link href="/projects">
+                  <Link href="/blog">
                     <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
                       <span className="flex items-center space-x-5">
                         <svg
@@ -95,10 +95,10 @@ export default function Home({ posts }) {
                             d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
                           />
                         </svg>
-                        <span className="pr-6 text-gray-900 dark:text-gray-100">What I built</span>
+                        <span className="pr-6 text-gray-900 dark:text-gray-100">What I wrote</span>
                       </span>
                       <span className="pl-6 text-amber-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
-                        Projects&nbsp;&rarr;
+                        All Posts &nbsp;&rarr;
                       </span>
                     </span>
                   </Link>
@@ -107,7 +107,7 @@ export default function Home({ posts }) {
               <div className="my-2 grid items-start gap-8">
                 <div className="group relative">
                   <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-fuchsia-600 to-emerald-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <Link href="https://parthdesai.vercel.app/">
+                  <Link href="/projects">
                     <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
                       <span className="flex items-center space-x-5">
                         <svg
@@ -124,19 +124,19 @@ export default function Home({ posts }) {
                             d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                           />
                         </svg>
-                        <span className="pr-6 text-gray-900 dark:text-gray-100">Read my story</span>
+                        <span className="pr-6 text-gray-900 dark:text-gray-100">What I built</span>
                       </span>
                       <span className="pl-6 text-indigo-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
-                        Website&nbsp;&rarr;
+                        Side Projects&nbsp;&rarr;
                       </span>
                     </span>
                   </Link>
                 </div>
               </div>
-              {/* <div className="my-2 grid items-start gap-8">
+              <div className="my-2 grid items-start gap-8">
                 <div className="group relative">
                   <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r  from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <Link href="https://parthdesai.vercel.app/resume.pdf">
+                  <Link href="https://baseonzero.vercel.app/about">
                     <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
                       <span className="flex items-center space-x-5">
                         <svg
@@ -156,16 +156,16 @@ export default function Home({ posts }) {
                           />
                         </svg>
                         <span className="pr-6 text-gray-900 dark:text-gray-100">
-                          Hire me!&nbsp;&nbsp;&nbsp;
+                          Read my story&nbsp;&nbsp;&nbsp;
                         </span>
                       </span>
                       <span className="pl-6 text-primary-400 transition duration-200 group-hover:text-gray-900 dark:group-hover:text-gray-100">
-                        Resume&nbsp;&rarr;
+                        About me&nbsp;&rarr;
                       </span>
                     </span>
                   </Link>
                 </div>
-              </div> */}
+              </div>
             </div>
           </div>
         </div>

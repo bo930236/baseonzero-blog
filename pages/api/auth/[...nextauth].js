42e7c85c82ab92fca05b0a5ddc5e56beb5a854e4
bobo <bo930236@gmail.com>
2023-04-11 13:35:00 +0800

    update prisma & delete claphands & upgrate dependencies

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
   session: {
@@ -11,10 +10,6 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.OAUTH_CLIENT_KEY,
       clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 })
